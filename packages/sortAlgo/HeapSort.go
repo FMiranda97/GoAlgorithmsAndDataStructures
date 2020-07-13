b021db70fef73c720e902346480836e3a819f71b
Rita00 <rita.lapao00@gmail.com>
2020-07-13 17:05:18 +0000

    shell and heap sort

---
+++ packages/sortAlgo/HeapSort.go
@@ -0,0 +1,43 @@
+package sortAlgo
+
+import "reflect"
+
+// Function to start heap sort in slice
+func HeapSort(arr interface{}) (e error) {
+	defer panicControl(&e)
+	if slice, swap, err := sortSetup(arr); err == nil {
+		heapSort(slice, swap)
+		return err
+	} else {
+		return err
+	}
+}
+
+// Utility function to perform heap sort in slice
+func heapSort(slice reflect.Value, swap func(int, int)) {
+	for i := slice.Len()/2 - 1; i >= 0; i-- {
+		heapify(slice, slice.Len(), i, swap)
+	}
+	for i := slice.Len() - 1; i > 0; i-- {
+		swap(0, i)
+		heapify(slice, i, 0, swap)
+	}
+}
+
+func heapify(slice reflect.Value, tam int, index int, swap func(int, int)) {
+	largest := index
+	left := 2*index + 1
+	right := 2*index + 2
+
+	if left < tam && get(left, slice).CompareTo(get(largest, slice)) > 0 {
+		largest = left
+	}
+	if right < tam && get(right, slice).CompareTo(get(largest, slice)) > 0 {
+		largest = right
+	}
+
+	if largest != index {
+		swap(index, largest)
+		heapify(slice, tam, largest, swap)
+	}
+}
---
+++ packages/sortAlgo/HeapSort_test.go
@@ -0,0 +1,22 @@
+package sortAlgo
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestHeapSort(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping testing in short mode")
+	}
+	err := HeapSort(invalidArray[:])
+	if err == nil {
+		t.Errorf("did not report error on invalid input")
+	}
+	var p [sizeEfficient]pessoa
+	copy(p[:], randomArray[:])
+	_ = HeapSort(p[:])
+	if !reflect.DeepEqual(sortedArray, p) {
+		t.Errorf("sort incorrect")
+	}
+}
